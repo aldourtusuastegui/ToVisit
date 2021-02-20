24405fd2939aa4ea2651fd9cfaaf11a87eebf233
Aldo Urtusuastegui <aldithomagno@gmail.com>
2021-02-19 20:44:25 -0700

    ADD adaptador para lista de interviews

--- app/src/main/java/com/acsoft/tovisit/ui/InterviewFragment.kt
+++ app/src/main/java/com/acsoft/tovisit/ui/InterviewFragment.kt
@@ -1,13 +1,45 @@
 package com.acsoft.tovisit.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.acsoft.tovisit.R
+import com.acsoft.tovisit.data.local.AppDatabase
+import com.acsoft.tovisit.data.local.LocalInterviewDataSource
+import com.acsoft.tovisit.data.model.InterviewItemEntity
+import com.acsoft.tovisit.data.remote.RemoteInterviewDataSource
+import com.acsoft.tovisit.data.remote.RetrofitClient
+import com.acsoft.tovisit.databinding.FragmentInterviewBinding
+import com.acsoft.tovisit.presentation.InterviewModelFactory
+import com.acsoft.tovisit.presentation.InterviewViewModel
+import com.acsoft.tovisit.repository.InterviewRepositoryImpl
+import com.acsoft.tovisit.ui.adapters.InterviewAdapter
+import kotlinx.coroutines.launch
 
-class InterviewFragment : Fragment() {
+class InterviewFragment : Fragment(),InterviewAdapter.OnInterviewClickListener {
+
+    private lateinit var binding : FragmentInterviewBinding
+
+    private lateinit var adapter : InterviewAdapter
+
+    private val viewModel by viewModels<InterviewViewModel> {
+        InterviewModelFactory(InterviewRepositoryImpl(
+                requireContext(),
+                RemoteInterviewDataSource(RetrofitClient.webService),
+                LocalInterviewDataSource(AppDatabase.getDatabase(requireContext()).interviewDao())
+        ))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = InterviewAdapter(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +48,32 @@ class InterviewFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_interview, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentInterviewBinding.bind(view)
+
+        showInterviews()
+    }
+
+    private fun showInterviews() {
+        binding.rvInterview.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvInterview.adapter = adapter
+
+        lifecycleScope.launch {
+            viewModel.getlist().observe(requireActivity(),{ list->
+                list.let {
+                    if(list.isNotEmpty()) {
+                        adapter.setInterviewList(list)
+                    } else {
+                        Log.d("TAG","vacio")
+                    }
+
+                }
+            })
+        }
+    }
+
+    override fun onInterviewClick(account: InterviewItemEntity) {
+        TODO("Not yet implemented")
+    }
 }
---
+++ app/src/main/java/com/acsoft/tovisit/ui/adapters/InterviewAdapter.kt
@@ -0,0 +1,61 @@
+package com.acsoft.tovisit.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.acsoft.tovisit.core.BaseViewHolder
+import com.acsoft.tovisit.data.model.InterviewItemEntity
+import com.acsoft.tovisit.databinding.InterviewItemBinding
+
+class InterviewAdapter(private val itemClickListener: OnInterviewClickListener) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    private var interviewList = listOf<InterviewItemEntity>()
+
+    fun setInterviewList(interviewList: List<InterviewItemEntity>) {
+        this.interviewList = interviewList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+
+        val itemBinding =
+                InterviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        val holder = InterviewViewHolder(itemBinding, parent.context)
+
+        itemBinding.root.setOnClickListener {
+            val position = holder.adapterPosition.takeIf { it != DiffUtil.DiffResult.NO_POSITION }
+                    ?: return@setOnClickListener
+            itemClickListener.onInterviewClick(interviewList[position])
+        }
+
+        return holder
+
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when(holder) {
+            is InterviewViewHolder -> {
+                holder.bind(interviewList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return interviewList.size
+    }
+
+    private inner class InterviewViewHolder(val binding : InterviewItemBinding, val context: Context) :
+            BaseViewHolder<InterviewItemEntity>(binding.root) {
+        override fun bind(item: InterviewItemEntity) {
+            binding.tvText.text = item.streetName
+        }
+    }
+
+    interface OnInterviewClickListener {
+        fun onInterviewClick(account: InterviewItemEntity)
+    }
+
+}
